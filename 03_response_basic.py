7cb97908a3dff14430156f726392e9d010fed269
shishirRsiam <shishir.siam01@gmail.com>
2025-05-08 01:35:08 +0600

    add 03_response_basic

--- 03_response_basic.py
+++ 03_response_basic.py
@@ -3,12 +3,13 @@ import requests
 url = 'https://books.toscrape.com/'
 response = requests.get(url)
 
-print("Status Code:", response.status_code)         # 200 = OK
-print("OK?:", response.ok)                           # True if status_code < 400
-print("Final URL:", response.url)                    # In case of redirect
-print("Encoding:", response.encoding)                # Usually 'utf-8'
-print("Headers:", response.headers)                  # responseponse headers
+print("Status Code:", response.status_code)          # HTTP status code (e.g., 200, 404, 500)
+print("OK?:", response.ok)                           # True if status code is < 400 (shortcut for checking success)
+print("Final URL:", response.url)                    # Final URL (useful after redirects), In case of redirect
+print("Encoding:", response.encoding)                # Encoding used to decode res.text (like 'utf-8')
+# print("Content:", response.content)                # Content in bytes (useful for images, files)
+print("Headers:", response.headers)                  # Dictionary of HTTP response headers
 
 
 print("\n\nFirst 300 chars of responseponse body:\n")
-print(response.text[:300])                           # HTML body as string
+print(response.text[:300])                           # Content of the response as a string (usually HTML or JSON)
